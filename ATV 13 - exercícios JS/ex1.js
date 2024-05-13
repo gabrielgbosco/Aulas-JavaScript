36a378d64a691581acacb21dc9bcf12da7fbe011
gabrielgbosco <160626981+gabrielgbosco@users.noreply.github.com>
2024-05-13 12:25:01 -0300

    Add files via upload

---
+++ ATV 13 - exercícios JS/ex1.js
@@ -0,0 +1,35 @@
+let a = 15;
+let b = 5;
+
+function diferenca() {
+    let resultado = a - b;
+    console.log (`A diferença dos números é: ${resultado}.`);
+}
+
+function calculoDeDobroETriplo() {
+    let somaPrimeiraSegunda = a * 2 + b * 3;
+    console.log (`A soma do dobro da primeira mais o triplo da segunda variável é igual a: ${somaPrimeiraSegunda}.`);
+}
+
+function multiplicarVariaveis() {
+    let multiplicacao = a * b;
+    console.log (`A multiplicação das variáveis é igual a: ${multiplicacao}.`);
+}
+
+function descobrirMaiorNumero() {
+    let maiorNumero = a;
+    let menorNumero = b;
+
+    if (b > a) {
+        maiorNumero = b;
+        menorNumero = a;
+    }
+
+    console.log (`O maior número é: ${maiorNumero}. E o menor número é: ${menorNumero}.`);
+    console.log (`Na ordem decrescente fica: ${maiorNumero}, ${menorNumero}.`);
+}
+
+diferenca()
+calculoDeDobroETriplo()
+multiplicarVariaveis()
+descobrirMaiorNumero()
